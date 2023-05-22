f5a1dcbb070b753a54a3fc42f7480e53a46b86ad
i am doraemon <khunball46@gmail.com>
2023-05-22 09:51:38 +0700

    check admin status

--- routes/roles/byAdmin/getAllRole.php
+++ routes/roles/byAdmin/getAllRole.php
@@ -5,11 +5,25 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/role/show/all', function (Request $request, Response $response) {
-        $sql = "SELECT * FROM role";
+    $app->get('/role/show/all/{id}', function (Request $request, Response $response, array $args) {
+        $id = $args['id'];
 
-        $run = new GetAll($sql, $response);
+        $sql = "SELECT * FROM member WHERE M_id = '$id'";
+        $run = new Get($sql, $response);
         $run->evaluate();
-        return $run->return();
+        $result = $run->getterResult();
+        
+        if ($result->M_admin) {
+            $sql = "SELECT * FROM role";
+
+            $run = new GetAll($sql, $response);
+            $run->evaluate();
+            return $run->return();
+        } else {
+            $response->getBody()->write(json_encode("You are not admin"));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(403);
+        }
     });
 };
