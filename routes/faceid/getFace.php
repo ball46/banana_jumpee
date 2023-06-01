8dd4317a68c8f9a63753eddb763a97b083261185
ball46 <khunball46@gmail.com>
2023-06-01 11:39:01 +0700

    to get check today who check in to start work

---
+++ routes/faceid/getFace.php
@@ -0,0 +1,11 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/face/get/data/history', function (Request $request, Response $response) {
+
+    });
+};
