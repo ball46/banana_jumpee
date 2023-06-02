97dccdfa115432dd634ba5b0d478665ebc6f7f43
i am doraemon <khunball46@gmail.com>
2023-06-02 10:49:32 +0700

    add new routes

--- routes/faceid/face.php
+++ routes/faceid/face.php
@@ -10,4 +10,12 @@ return function (App $app) {
     //check last time
     $routes = require __DIR__ . '/checkLastTime.php';
     $routes($app);
+
+    //check start work for every day
+    $routes = require __DIR__ . '/countFaceOnCurrentDate.php';
+    $routes($app);
+
+    //check start work foreach members
+    $routes = require __DIR__ . '/countFaceOnCurrentMonth.php';
+    $routes($app);
 };
