410348694f20cea14aa74944c59a0f572eeef0da
i am doraemon <khunball46@gmail.com>
2023-06-02 10:51:29 +0700

    this is new route for use to see this member id start work on this month normal late absent

---
+++ routes/faceid/countFaceOnCurrentMonth.php
@@ -0,0 +1,46 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/face/count/face/current/month/{member_id}',
+        function (Request $request, Response $response, array $args) {
+        $member_id = $args['member_id'];
+        date_default_timezone_set('Asia/Bangkok');
+        $current_timestamp = time();
+        $now_month = date("m", $current_timestamp);
+
+        $sql = "SELECT * FROM faceid 
+                WHERE MONTH(F_date) = $now_month AND F_in_out = '1' AND F_member_id = '$member_id'";
+        $run = new GetAll($sql, $response);
+        $run->evaluate();
+        $all = $run->getterCount();
+        $normal = 0;
+        $late = 0;
+        $absent = 0;
+        if($all){
+            $data_current_date = $run->getterResult();
+            foreach ($data_current_date as $data){
+                if($data->F_work == "normal" || $data->F_work == "normal_leave" || $data->F_work == "OT"){
+                    $normal++;
+                }else if($data->F_work == "late"){
+                    $late++;
+                }else{
+                    $absent++;
+                }
+            }
+        }
+        $data_current_date = array(
+            'all' => $all,
+            'normal' => $normal,
+            'late' => $late,
+            'absent' => $absent,
+        );
+        $response->getBody()->write(json_encode($data_current_date));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    });
+};
