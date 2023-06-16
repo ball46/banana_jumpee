d840ef89b8055adc7d9058990d81148ff849b47b
i am doraemon <khunball46@gmail.com>
2023-06-16 14:25:53 +0700

    maintain

--- routes/faceid/timeInOutWork.php
+++ routes/faceid/timeInOutWork.php
@@ -16,8 +16,8 @@ return function (App $app) {
         $now_date = date("Y-m-d", $current_timestamp);
 
         $time = array(
-            'time-in' => "",
-            'time-out' => ""
+            'timeIn' => "none",
+            'timeOut' => "none"
         );
 
         $sql = "SELECT * FROM faceid WHERE F_member_id = '$member_id' AND F_date = '$now_date'";
@@ -27,9 +27,9 @@ return function (App $app) {
             $data_history = $run->getterResult();
             foreach ($data_history as $data){
                 if($data->F_in_out){
-                    $time['time-in'] = $data->F_time;
+                    $time['timeIn'] = $data->F_time;
                 }else{
-                    $time['time-out'] = $data->F_time;
+                    $time['timeOut'] = $data->F_time;
                 }
             }
         }
