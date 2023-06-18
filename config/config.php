b87ef36826c18290faa5f2cb8cdf0854405846b5
i am doraemon <khunball46@gmail.com>
2023-06-18 14:39:09 +0700

    new sort

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/db.php';
-require __DIR__ . '/update.php';
 require __DIR__ . '/get.php';
 require __DIR__ . '/getAll.php';
+require __DIR__ . '/update.php';
 require __DIR__ . '/work.php';
