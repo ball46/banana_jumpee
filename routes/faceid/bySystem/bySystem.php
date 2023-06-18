728e585ed409b5aed01f6a680768d104bf4cabe6
i am doraemon <khunball46@gmail.com>
2023-06-18 15:10:09 +0700

    split path folder to easily search

---
+++ routes/faceid/bySystem/bySystem.php
@@ -0,0 +1,21 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    //check last time
+    $routes = require __DIR__ . '/checkLastTime.php';
+    $routes($app);
+
+    //check start work for every day
+    $routes = require __DIR__ . '/countFaceOnCurrentDate.php';
+    $routes($app);
+
+    //check start work foreach members
+    $routes = require __DIR__ . '/countFaceOnMonth.php';
+    $routes($app);
+
+    //get time in out work in current date
+    $routes = require __DIR__ . '/timeInOutWork.php';
+    $routes($app);
+};
--- routes/faceid/face.php
+++ routes/faceid/face.php
@@ -3,23 +3,11 @@
 use Slim\App;
 
 return function (App $app) {
-    //check in by scan face id
-    $routes = require __DIR__ . '/postFace.php';
+    //call api in folder bySystem
+    $routes = require __DIR__ . '/byDevice/byDevice.php';
     $routes($app);
 
-    //check last time
-    $routes = require __DIR__ . '/checkLastTime.php';
-    $routes($app);
-
-    //check start work for every day
-    $routes = require __DIR__ . '/countFaceOnCurrentDate.php';
-    $routes($app);
-
-    //check start work foreach members
-    $routes = require __DIR__ . '/countFaceOnMonth.php';
-    $routes($app);
-
-    //get time in out work in current date
-    $routes = require __DIR__ . '/timeInOutWork.php';
+    //call api in folder bySystem
+    $routes = require __DIR__ . '/bySystem/bySystem.php';
     $routes($app);
 };
