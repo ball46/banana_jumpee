bb1b19894d71020e43baf3d90c6e22ed81fbb037
i am doraemon <khunball46@gmail.com>
2023-06-19 21:03:01 +0700

    maintain it to have checked rest day in role id it clouds have more than 1 day

--- routes/faceid/bySystem/checkLastTime.php
+++ routes/faceid/bySystem/checkLastTime.php
@@ -7,7 +7,6 @@ use Slim\App;
 return function (App $app) {
     $app->put('/face/check/last', function (Request $request, Response $response) {
         //create time by php
-        //TODO:change check rest day to have many day is not 1 day
         date_default_timezone_set('Asia/Bangkok');
         $current_timestamp = time();
         $scan_date = date("Y-m-d", $current_timestamp);
@@ -23,6 +22,7 @@ return function (App $app) {
         $work = "";
         $in_out = 1;
 
+        //to check today is holiday or not
         $sql = "SELECT * FROM holiday 
                 WHERE H_start_date <= '$scan_date' AND H_end_date >= '$scan_date' AND H_status = '1'";
         $run = new Get($sql, $response);
@@ -36,6 +36,20 @@ return function (App $app) {
             foreach ($members as $member) {
                 $member_id = $member->M_id;
                 $role_id = $member->M_role_id;
+
+                $sql = "SELECT * FROM role WHERE R_id = '$role_id'";
+                $run = new Get($sql, $response);
+                $run->evaluate();
+                $data_role = $run->getterResult();
+                $rest_days = explode(" ", $data_role->R_rest_day);
+                $rest_or_absent = 0;
+                foreach ($rest_days as $rest_day) {
+                    if ($day_name == $rest_day) {
+                        $rest_or_absent = 1;
+                        break;
+                    }
+                }
+
                 $sql = "SELECT * FROM faceid WHERE F_member_id = '$member_id' AND F_date = '$scan_date' 
                     ORDER BY F_id DESC LIMIT 2";
                 $run = new GetAll($sql, $response);
@@ -52,14 +66,17 @@ return function (App $app) {
                         $end_date = $data_leave->V_end_date;
                         if ($scan_date >= $start_date && $scan_date <= $end_date) {
                             continue;
-                        } else {
-                            $in_out = 0;
-                            $work = "absent";
                         }
-                    } else {
-                        $in_out = 0;
-                        $work = "absent";
                     }
+                    $in_out = 0;
+                    $work = "absent";
+
+                    $cal = new Work($member_id, 0, $in_out, "by api", "by api",
+                        $day_name, $scan_date, $scan_time, $scan_timestamp, $scan_timestamp, $work);
+                    $cal->first_scan();
+                    $sql = $cal->getterSQL();
+                    $run = new Update($sql, $response);
+                    $run->evaluate();
                 } else {
                     if ($member->M_leave) {
                         $sql = "SELECT * FROM vacation WHERE V_member_id = '$member_id' AND V_status = '1'";
@@ -70,46 +87,16 @@ return function (App $app) {
                         $end_date = $data_leave->V_end_date;
                         if ($scan_date >= $start_date && $scan_date <= $end_date) {
                             $work = $data_leave->V_time_period == "all day" ? "leave" : "absent";
-                        } else {
-                            if ($member->M_profiling) {
-                                $sql = "SELECT * FROM datework WHERE D_member_id = '$member_id' AND D_status = '1'";
-                                $run = new Get($sql, $response);
-                                $run->evaluate();
-                                $data_date_work = $run->getterResult();
-                                $start_date = $data_date_work->D_start_date_work;
-                                $end_date = $data_date_work->D_end_date_work;
-                                if ($scan_date >= $start_date && $scan_date <= $end_date) {
-                                    if ($data_date_work->D_choose_date_name) {
-                                        $days = explode(" ", $data_date_work->D_date_name);
-                                        $date = $day_name;
-                                    } else {
-                                        $days = explode(" ", $data_date_work->D_date_num);
-                                        $date = date("d", $current_timestamp);
-                                    }
-                                    foreach ($days as $day) {
-                                        if ($day == $date) {
-                                            $work = "absent";
-                                            break;
-                                        } else {
-                                            $work = "profiling";
-                                        }
-                                    }
-                                } else {
-                                    $sql = "SELECT * FROM role WHERE R_id = '$role_id'";
-                                    $run = new Get($sql, $response);
-                                    $run->evaluate();
-                                    $data_role = $run->getterResult();
-                                    $work = $data_role->R_rest_day == $day_name ? "rest day" : "absent";
-                                }
-                            } else {
-                                $sql = "SELECT * FROM role WHERE R_id = '$role_id'";
-                                $run = new Get($sql, $response);
-                                $run->evaluate();
-                                $data_role = $run->getterResult();
-                                $work = $data_role->R_rest_day == $day_name ? "rest day" : "absent";
-                            }
                         }
-                    } else if ($member->M_profiling) {
+                        $cal = new Work($member_id, 0, $in_out, "by api", "by api",
+                            $day_name, $scan_date, $scan_time, $scan_timestamp, $scan_timestamp, $work);
+                        $cal->first_scan();
+                        $sql = $cal->getterSQL();
+                        $run = new Update($sql, $response);
+                        $run->evaluate();
+                        continue;
+                    }
+                    if ($member->M_profiling) {
                         $sql = "SELECT * FROM datework WHERE D_member_id = '$member_id' AND D_status = '1'";
                         $run = new Get($sql, $response);
                         $run->evaluate();
@@ -132,19 +119,16 @@ return function (App $app) {
                                     $work = "profiling";
                                 }
                             }
-                        } else {
-                            $sql = "SELECT * FROM role WHERE R_id = '$role_id'";
-                            $run = new Get($sql, $response);
-                            $run->evaluate();
-                            $data_role = $run->getterResult();
-                            $work = $data_role->R_rest_day == $day_name ? "rest day" : "absent";
                         }
-                    } else {
-                        $sql = "SELECT * FROM role WHERE R_id = '$role_id'";
-                        $run = new Get($sql, $response);
+                        $cal = new Work($member_id, 0, $in_out, "by api", "by api",
+                            $day_name, $scan_date, $scan_time, $scan_timestamp, $scan_timestamp, $work);
+                        $cal->first_scan();
+                        $sql = $cal->getterSQL();
+                        $run = new Update($sql, $response);
                         $run->evaluate();
-                        $data_role = $run->getterResult();
-                        $work = $data_role->R_rest_day == $day_name ? "rest day" : "absent";
+                        continue;
+                    } else {
+                        $work = $rest_or_absent ? "rest day" : "absent";
                     }
                     $cal = new Work($member_id, 0, $in_out, "by api", "by api",
                         $day_name, $scan_date, $scan_time, $scan_timestamp, $scan_timestamp, $work);
